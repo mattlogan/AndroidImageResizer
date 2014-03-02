ea5e7478d2d0aa2acd6c4e68ab8ed3b0d79f7f0a
mattlogan <matt.b.logan@gmail.com>
2014-03-02 14:24:32 -0800

    initial commit

---
+++ AndroidImageResizer.js
@@ -0,0 +1,60 @@
+var fs = require('fs');
+var path = require('path');
+var imagemagick = require('imagemagick');
+var readline = require('readline');
+
+var sizeNames = ['xxhdpi', 'xhdpi', 'hdpi', 'mdpi'];
+
+for (var i = 0; i < sizeNames.length; i++) {
+  fs.mkdirSync(sizeNames[i], function(err) {
+    if(err) throw err;
+  });
+}
+
+var currentDirectory = process.cwd();
+var allFiles = fs.readdirSync(currentDirectory);
+
+var imageFiles = [];
+for (var i = 0; i < allFiles.length; i++) {
+  var extname = path.extname(allFiles[i]);
+  if (extname == '.jpg' || extname == '.jpeg' || extname == '.png') {
+    imageFiles.push(allFiles[i]);
+  }
+}
+
+for (var i = 0; i < imageFiles.length; i++) {
+  for (var j = 0; j < sizeNames.length; j++) {
+    resize(imageFiles[i], sizeNames[j]);
+  }
+}
+
+function resize(fileName, sizeName) {
+  imagemagick.convert([fileName, '-resize',
+      getMultiplierString(sizeName), sizeName + '/' + fileName]);
+}
+
+function getMultiplierString(sizeName) {
+	return (getMultiplier(sizeName) * 100) + '%';
+}
+
+function getMultiplier(sizeName) {
+  var multiplier;
+    switch (sizeName) {
+      case 'xxhdpi':
+        multiplier = 3.0 / 3.0;
+        break;
+      case 'xhdpi':
+        multiplier = 2.0 / 3.0;
+        break;
+      case 'hdpi':
+        multiplier = 1.5 / 3.0;
+        break;
+      case 'mdpi':
+        multiplier = 1.0 / 3.0;
+        break;
+      default:
+        multiplier = 1;
+        break;
+  }
+  return multiplier;
+}
