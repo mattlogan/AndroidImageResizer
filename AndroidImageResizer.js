e72858e21f3e96c323695d78b6cb971ac6fd4a90
mattlogan <matt.b.logan@gmail.com>
2014-03-17 18:49:11 -0700

    don't need require('readline')

--- AndroidImageResizer.js
+++ AndroidImageResizer.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var path = require('path');
 var imagemagick = require('imagemagick');
-var readline = require('readline');
 
 var sizeNames = ['xxhdpi', 'xhdpi', 'hdpi', 'mdpi'];
 
